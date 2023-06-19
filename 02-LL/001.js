b70d818c42bc101ae422d8cba4f9fc4ec25dd4bb
immayurpanchal <mayurpanchal002@gmail.com>
2023-06-19 08:33:54 +0530

    1 file modified

--- 02-LL/001.js
+++ 02-LL/001.js
@@ -2,13 +2,13 @@ class Node {
   constructor(ele) {
     this.next = null;
     this.value = ele;
-    this.size = 0;
   }
 }
 
 class LinkedList {
   constructor() {
     this.head = null;
+    this.size = 0;
   }
 
   push(ele) {
@@ -21,7 +21,7 @@ class LinkedList {
     }
 
     // traverse to the end of the linked list
-    const temp = this.head;
+    let temp = this.head;
     while (temp.next !== null) {
       temp = temp.next;
     }
@@ -33,9 +33,103 @@ class LinkedList {
 
   pop() {
     // if list is empty
+    if (!this.head) {
+      throw Error('Can not delete item from empty LL');
+    }
+
+    let temp = this.head;
+
+    // if there is only one item
+    if (temp.next === null) {
+      this.head = null;
+      --this.size;
+      return temp;
+    }
+
+    // traverse to second last element
+    if (temp.next !== null && temp.next.next !== null) {
+      temp = temp.next;
+    }
+
+    temp.next = null;
+    --this.size;
+    return temp;
   }
-  insertAt() {}
-  delete() {}
+
+  insertAt(index, ele) {
+    // insert outside the limit
+    if (index < 0 || index > this.size) {
+      throw Error('index out of bound');
+    }
+    // insert at start
+    // when the list is empty
+    if (index === 0) {
+      const node = new Node(ele);
+
+      if (this.head === null) {
+        this.head = node;
+        ++this.size;
+        return;
+      }
+
+      node.next = this.head;
+      this.head = node;
+      ++this.size;
+      return;
+    }
+
+    // when there is one item and index is 1
+    let temp = this.head;
+    if (index === 1 && temp.next === null) {
+      const node = new Node(ele);
+      temp.next = node;
+      ++this.size;
+      return;
+    }
+    // when there are lots of items
+    // traverse to the element index - 1
+
+    const node = new Node(ele);
+    for (let i = 0; i < index - 1; i++) {
+      temp = temp.next;
+    }
+    node.next = temp.next;
+    temp.next = node;
+    ++this.size;
+  }
+
+  delete(ele) {
+    if (!this.head) {
+      throw Error('Can not delete from an empty list'); // if the list is empty, return null
+    }
+
+    if (this.head.value === ele) {
+      this.head = this.head.next; // if the key is at the head node of the list, remove it by updating the head pointer
+      --this.size;
+      return;
+    }
+
+    // Because,  first element is not the value we're searching
+    // Hence, start from the second element
+    let prevNode = this.head;
+    let currNode = prevNode.next;
+
+    while (currNode !== null) {
+      if (currNode.value === ele) {
+        prevNode.next = currNode.next; // if the key is found at a non-head node, remove it by updating the previous node's next pointer
+        // free memory of removed node
+        currNode = null;
+        --this.size;
+        return;
+      }
+
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+
+    throw Error('Element does not exist'); // if the key is not found in the list, return null
+  }
+
   print() {
     if (this.head === null) {
       console.log('LL is empty');
@@ -53,5 +147,9 @@ const ll = new LinkedList();
 ll.push(10);
 ll.push(20);
 ll.push(30);
-console.log(this.size);
+// ll.print();
+// console.log('Deleted Ele :', ll.pop());
+// ll.insertAt(0, 50);
+// ll.delete(10);
+console.log('Size', ll.size);
 ll.print();
