01005e2a5261b1bc93c6d10e7287e78083f9e2bb
immayurpanchal <mayurpanchal002@gmail.com>
2023-07-05 09:52:07 +0530

    1 file modified

--- 04-Stack/003.js
+++ 04-Stack/003.js
@@ -13,10 +13,21 @@ function increasing(arr) {
   console.log(stack);
 }
 
-function decreasing() {}
+function decreasing(arr) {
+  const stack = new Stack();
+
+  arr.forEach((item) => {
+    while (!stack.isEmpty() && stack.peek() < item) {
+      stack.pop();
+    }
+    stack.push(item);
+  });
+
+  console.log(stack);
+}
 
 const arr = [1, 4, 5, 3, 12, 10]; // OP: 1, 3, 10
-const arr2 = [17, 14, 10, 5, 1]; // OP: 17, 14, 10
+const arr2 = [15, 17, 12, 13, 14, 10]; // OP: 17, 14, 10
 
 increasing(arr);
-// decreasing(arr2)
+decreasing(arr2);
