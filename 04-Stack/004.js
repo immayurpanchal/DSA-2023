aee457671c865447cbe2d8fb09c9321c0d733d7f
immayurpanchal <mayurpanchal002@gmail.com>
2023-07-11 12:36:06 +0530

    2 files modified

--- 04-Stack/004.js
+++ 04-Stack/004.js
@@ -1,28 +1,43 @@
 import { StackLL } from './002';
 import { Stack } from './001';
 
-// reverse a string using Stack linked list implementation
+// Reverse a string using a Stack implemented with a singly linked list
 function reverseStringStackLL(str) {
+  // Create a new StackLL instance
   const stack = new StackLL();
+
+  // Push each character of the input string onto the stack
   for (let i = 0; i < str.length; i++) {
     stack.push(str[i]);
   }
+
+  // Pop each character off the stack and construct the reversed string
   let reversedStr = '';
   while (!stack.isEmpty()) {
     reversedStr += stack.pop();
   }
+
+  // Return the reversed string
   return reversedStr;
 }
 
+// Reverse a string using a Stack implemented with a singly linked list
 function reverseStringStack(str) {
-  const stack = new Stack();
+  // Create a new StackLL instance
+  const stack = new StackLL();
+
+  // Push each character of the input string onto the stack
   for (let i = 0; i < str.length; i++) {
     stack.push(str[i]);
   }
+
+  // Pop each character off the stack and construct the reversed string
   let reversedStr = '';
   while (!stack.isEmpty()) {
     reversedStr += stack.pop();
   }
+
+  // Return the reversed string
   return reversedStr;
 }
 
